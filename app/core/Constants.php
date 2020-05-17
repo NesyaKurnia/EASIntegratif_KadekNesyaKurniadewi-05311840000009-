7bc738fb017520f50ed5d4a322887faf12b5bd00
NesyaKurnia <50094449+NesyaKurnia@users.noreply.github.com>
2020-05-17 22:48:24 +0800

    Add files via upload

---
+++ app/core/Constants.php
@@ -0,0 +1,8 @@
+<?php
+
+define("BASE_URL", 'http://localhost/integratif/eas/public');
+
+define("DB_HOST", "localhost");
+define("DB_NAME", "integratif");
+define("DB_USERNAME", "integratifeas");
+define("DB_PASSWORD", "integratif123");
